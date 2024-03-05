682603bcb7df5368f02e2a1681c3181f580ee981
Zsombor Szenyán <zsomborszenyan@gmail.com>
2024-03-05 20:50:49 +0100

    Initial Commit

---
+++ iFace/iFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFaceApp.swift
+//  iFace
+//
+//  Created by Zsombor Szenyán on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
